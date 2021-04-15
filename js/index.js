c2e941b45f48376d1308c8145c0454dbb5ec5ade
Maria Alice <malice.rfort@gmail.com>
2021-04-15 17:17:26 -0300

    primeiro commit

---
+++ js/index.js
@@ -0,0 +1,67 @@
+function monica(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block' 
+
+    mostrar.innerHTML = "<img src='css/img/monica.png'>"
+    mostrar.innerHTML += '<p>Monica Geller (Courteney Cox). </p>'
+    mostrar.innerHTML += "<p> É irmã do Ross e amiga de Rachel do tempo de escola. Ama limpeza e trabalha como chef de cozinha.</p> "
+    mostrar.innerHTML += "<p><strong>Frases:</strong> 'Normalmente quando estou ansiosa assim, eu limpo' <br> 'Bem-vinda ao mundo real. É uma droga. Você vai amar'</p>"
+    mostrar.innerHTML += "<p> <a href='https://friends.fandom.com/wiki/Monica_Geller' target='_blank'> Saiba mais! </a></p>"
+    
+}
+function phoebe(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block'
+
+    mostrar.innerHTML = "<img src='css/img/phoebe.png'>"
+    mostrar.innerHTML += '<p>Phoebe Buffay (Lisa Kudrow).</p>'
+    mostrar.innerHTML += " <p>É massagista e tem uma vida familiar bem conturbada. Ela já viveu nas ruas, gerou os trigêmeos de um meio-irmão e ainda precisa lidar com uma irmã gêmea má.</p>"
+    mostrar.innerHTML += "<p><strong>Frases:  </strong> 'Oi, eu sou Regina Phalange. <br> 'Olá embriõezinhos. Eu sou Phoebe Buffay. Oi! Espero ser seu útero nos próximos nove meses'</p>"
+    mostrar.innerHTML += "<p> <a href='https://friends.fandom.com/wiki/Phoebe_Buffay' target='_blank'> Saiba mais! </a></p>"
+}
+function rachel(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block'
+
+    mostrar.innerHTML = "<img src='css/img/rachel.png'>"
+    mostrar.innerHTML += '<p>Rachel Green (Jennifer Aniston).</p>'
+    mostrar.innerHTML += "<p>Chega ao grupo como a patricinha do grupo, logo após desistir de um casamento e foi atrás dos seus sonhos na área da moda.</p> "
+    mostrar.innerHTML += "<p><strong>Frases:</strong> 'Posso ficar com os presentes e continuar com 29 anos?' <br> 'Vou buscar um desses…desses…empregos' <br> </p>"
+    mostrar.innerHTML += "<p><a href='https://friends.fandom.com/wiki/Rachel_Greene' target='_blank'> Saiba mais! </a></p>"
+}
+function joey(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block'
+
+    mostrar.innerHTML = "<img src='css/img/joey.png'>"
+    mostrar.innerHTML += '<p>Joey Tribbiani (Matt LeBlanc). </p>'
+    mostrar.innerHTML += "<p>É ator, apesar de não ter uma carreira muito boa. Por outro lado, tem uma vida romântica bem ativa, graças a seu charme irresistível.</p> "
+    mostrar.innerHTML += "<p><strong>Frases:</strong> 'Joey não divide comida' <br> 'Por que, Deus? Por quê? Nós tínhamos um acordo. Deixe que os outros fiquem velhos, não eu'</p>"
+    mostrar.innerHTML += "<p><a href='https://friends.fandom.com/wiki/Matt_LeBlanc' target='_blank'> Saiba mais! </a></p>"
+}
+function chandler(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block'
+ 
+    mostrar.innerHTML = "<img src='css/img/chandler.png'>"
+    mostrar.innerHTML += '<p>Chandler Bing (Matthew Perry). </p>'
+    mostrar.innerHTML += "<p>Está sempre contando piadas e fazendo graça para o grupo. Além disso, é filho de uma família nada tradicional com uma mãe autora de livros eróticos e um pai trans.</p>"
+    mostrar.innerHTML += "<p><strong>Frases:</strong> 'Eu sou o Chandler, eu faço piadas quando estou desconfortável' <br> 'Eu não sou muito bom em dar conselhos. Você não prefere um comentário sarcástico?'</p>"
+    mostrar.innerHTML += "<p><a href='https://friends.fandom.com/wiki/Matthew_Perry' target='_blank'> Saiba mais! </a></p>"
+}
+function ross(){
+    let mostrar = document.getElementById('res')
+
+    mostrar.style.display = 'block'
+
+    mostrar.innerHTML = "<img src='css/img/ross.png'>"
+    mostrar.innerHTML += '<p>Ross Geller (David Schwimmer).</p>'
+    mostrar.innerHTML += "<p>É irmão de Monica, paleontólogo e era apaixonado por Rachel na adolescência. Mais tarde, na faculdade, fica amigo de Chandler.</p>"
+    mostrar.innerHTML += "<p><strong>Frases:</strong> 'Eu troquei o basquete pela Paleontologia' <br> 'Grande surpresa, eu gosto de pedidos de casamento!'</p>"
+    mostrar.innerHTML += "<p><a href='https://friends.fandom.com/wiki/Ross_Geller' target='_blank'> Saiba mais! </a></p>"
+}
